485737c4b7d47403c817043f5b93e9214490f210
joshmackay <joshwademackay@gmail.com>
2022-06-27 17:36:56 +1000

    successful mouseover grid cells next to work on changing colour on mouseover

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+const GRID_WIDTH = document.querySelector('#grid').offsetWidth;
+const resetButton = document.querySelector('#reset-button').addEventListener('click', reset);
+
 function createGrid(size){ //function to create a grid based on row and col values
     for(let r = 0; r < size; r++){
         const row = document.createElement('div');
         row.className = 'row';
         
         for(let c = 0; c < size; c++){
-            let cellWidth = (500/size);
-            let cellHeight = (500/size);
+            let cellWidth = (GRID_WIDTH/size);
+            let cellHeight = (GRID_WIDTH/size);
             const cell = document.createElement('div');
-            cell.className = 'cell';
-            cell.style.backgroundColor = `rgb(${Math.floor( Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
+            cell.classList.add = 'cell';
+            cell.addEventListener('mouseover', toBlack(this));
             cell.style.width = cellWidth + 'px';
             cell.style.height = cellHeight + 'px';
             row.appendChild(cell);
@@ -17,4 +20,26 @@ function createGrid(size){ //function to create a grid based on row and col valu
     }
 }
 
-createGrid(100);
+function toBlack(x){
+    console.log(x);
+}
+
+function changeColour(){
+    console.log('fired');
+}
+
+function getRandomRGB(){
+    return `rgb(${Math.floor( Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`
+    
+}
+
+function reset(){
+    let existingElements = document.querySelector('#grid');
+    existingElements.replaceChildren();
+    createGrid(16)
+}
+
+document.querySelector('body').addEventListener('load', createGrid(16));
+
+console.log(GRID_WIDTH);
+
